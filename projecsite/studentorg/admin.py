a7ef48fb1a41739ff173521a382a62fbae404037
Laxusimfinite11 <laxustaladro@gmail.com>
2024-10-31 15:40:55 +0800

    added Programs list display: college, Students list display: get college

--- projecsite/studentorg/admin.py
+++ projecsite/studentorg/admin.py
@@ -4,21 +4,36 @@ from .models import College, Program, Organization, Student, OrgMember
 # Register your models here.
 
 admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("program",)
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college")
+    search_fields = ("organization",)
+
 
 @admin.register(Student)
-class OrgMemberAdmin(admin.ModelAdmin):
+class StudentAdmin(admin.ModelAdmin):
     list_display = ("student_id", "lastname", 
-                    "firstname", "middlename", "program")
-
+                    "firstname", "middlename", "program", "get_college")
     search_fields = ("lastname", "firstname",)
+
+    def get_college(self, obj):
+        try:
+            pro = Program.objects.get(prog_name=obj.program)
+            return pro.college
+
+        except Program.DoesNotExist:    
+            return None
 
 @admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
     list_display = ("student", "get_member_program", "organization",
                     "date_joined",)
-    
     search_fields = ("student_lastname", "student_firstname",)
 
     def get_member_program(self, obj):
